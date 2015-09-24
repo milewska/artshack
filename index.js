1452cf59670db64d01982bacf7cb9442d0793a56
Tyler Johnson <tyler@vintyge.com>
2015-09-23 18:58:41 -0600

    express stuff

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var app = require("express")();
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res, next) {
-
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/contours", function(req, res, next) {
+	console.log(req.body);
+	res.send("OK");
+});
 
+app.listen(3000, function() {
+	console.log("ready");
 });
