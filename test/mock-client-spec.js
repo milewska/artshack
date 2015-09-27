7a57e8d9eadb00e846ac6e66e26ad19841b22831
cesine <cesine@yahoo.com>
2015-09-26 19:06:29 -0600

    mocked data simulates change over time

--- test/mock-client-spec.js
+++ test/mock-client-spec.js
@@ -24,7 +24,7 @@ describe(" mock client", function() {
 			expect(client.generateData).toBeDefined();
 		});
 
-		it("should have a seed generator", function() {
+		it("should generator a seed set of data", function() {
 			var data = new MockClient().generateSeed({
 				width: 1096,
 				height: 800,
@@ -36,6 +36,60 @@ describe(" mock client", function() {
 			expect(data.length).toEqual(5);
 			data.map(function(vector) {
 				expect(vector.length <= 15 && vector.length >= 5).toBeTruthy();
+			});
+		});
+
+		it("should generate data", function() {
+			var data = new MockClient({
+				width: 1096,
+				height: 800,
+				jump: 10,
+				vectorCount: 5,
+				vectorMeanLength: 10,
+				curve: -3
+			}).generateData();
+
+			expect(data.length >= 4 && data.length <= 6).toBeTruthy();
+			data.map(function(vector) {
+				console.log(vector.length);
+				expect(vector.length = 1 || (vector.length <= 15 && vector.length >= 8)).toBeTruthy();
+			});
+		});
+
+		it("should generator data based on a seed", function() {
+			var currentFrame = [
+				[{
+					"x": 1024.0,
+					"y": 718.0
+				}],
+				[{
+					"x": 1017.0,
+					"y": 717.0
+				}, {
+					"x": 1016.0,
+					"y": 718.0
+				}, {
+					"x": 1015.0,
+					"y": 718.0
+				}, {
+					"x": 1016.0,
+					"y": 718.0
+				}, {
+					"x": 1017.0,
+					"y": 717.0
+				}, {
+					"x": 1018.0,
+					"y": 718.0
+				}]
+			];
+			var nextFrame = new MockClient().generateData(currentFrame);
+
+			console.log(nextFrame);
+			expect(nextFrame.length >= 2 && nextFrame.length <= 4).toBeTruthy();
+			nextFrame.map(function(vector) {
+				console.log(vector.length);
+
+				expect(vector.length <= 7 && vector.length >= 0).toBeTruthy();
 			});
 		});
 	});
--- test/mock-client.js
+++ test/mock-client.js
@@ -1,10 +1,67 @@
 "use strict";
 var request = require("request");
 
+var newRandomPoint = function(width, height) {
+	console.log(" newRandomPoint", width, height);
+	return {
+		x: Math.round(Math.random() * width),
+		y: Math.round(Math.random() * height)
+	};
+};
+
+var changePointByDelta = function(point, delta) {
+	console.log(" changePointByDelta", point, delta);
+	delta = delta || 10;
+	return {
+		x: Math.round(point.x + Math.random() * delta),
+		y: Math.round(point.y + Math.random() * delta)
+	};
+};
+
+var growOrShrinkVectors = function(vectors, newItem) {
+	if (Math.random() > 0.5) {
+		console.log(" growing from " + vectors.length);
+		if (Math.random() > 0.5) {
+			vectors.unshift(newItem);
+		} else {
+			vectors.push(newItem);
+		}
+	} else {
+		if (vectors.length) {
+			console.log(" shrinking from " + vectors.length);
+			if (Math.random() > 0.5) {
+				// Remove one from this vector or one of its children
+				if (vectors[0].length) {
+					vectors[0].shift();
+				} else {
+					vectors.shift();
+				}
+			} else {
+				if (vectors[vectors.length - 1].length) {
+					vectors[vectors.length - 1].pop();
+				} else {
+					vectors.pop();
+				}
+			}
+		}
+	}
+};
+
 var MockClient = function(options) {
 	console.log("Constructing a mock client ", options);
+	if (!options) {
+		options = {
+			width: 1096,
+			height: 800,
+			jump: 10,
+			vectorCount: 5,
+			vectorMeanLength: 10
+		};
+	}
 
 	return {
+		width: options.width || 1000,
+		height: options.height || 600,
 		timeout: 5000,
 		stop: false,
 		debug: function() {
@@ -27,15 +84,16 @@ var MockClient = function(options) {
 			});
 		},
 		generateData: function(vectors) {
+			var self = this;
 			if (!vectors) {
 				vectors = this.generateSeed();
 			}
+			// Make the vector grow or shrink
+			growOrShrinkVectors(vectors, [newRandomPoint(this.width, this.height)]);
 			return vectors.map(function(vector) {
+				growOrShrinkVectors(vector, newRandomPoint(self.width, self.height));
 				return vector.map(function(point) {
-					return {
-						x: point.x + Math.random(),
-						y: point.y + Math.random()
-					};
+					return changePointByDelta(point, options.jump);
 				});
 			});
 		},
@@ -45,39 +103,21 @@ var MockClient = function(options) {
 		 * @param  {Object} options Optiona parameters to geenerate data shapes
 		 * @return {Array}         
 		 */
-		generateSeed: function(options) {
-			if (!options) {
-				options = {
-					width: 1096,
-					height: 800,
-					jump: 10,
-					vectorCount: 5,
-					vectorMeanLength: 10
-				};
-			}
+		generateSeed: function() {
 			var data = [];
 			var vector;
 			var length;
 			var previousPoint;
-			var currentPoint;
 			var i, p;
 			for (i = 0; i < options.vectorCount; i++) {
 				vector = [];
-				length = (Math.random() * options.vectorMeanLength / 4) + options.vectorMeanLength / 2;
-				previousPoint = {
-					x: Math.round(Math.random() * options.width),
-					y: Math.round(Math.random() * options.height)
-				};
+				length = (Math.random() * options.vectorMeanLength / 4) + options.vectorMeanLength / 1.5;
+				previousPoint = newRandomPoint(this.width, this.height);
 				for (p = 0; p < length; p++) {
-					currentPoint = {
-						x: Math.round(previousPoint.x + Math.random() * options.jump),
-						y: Math.round(previousPoint.y + Math.random() * options.jump)
-					};
-					vector.push(currentPoint);
-					previousPoint = currentPoint;
+					previousPoint = changePointByDelta(previousPoint, options.jump);
+					vector.push(previousPoint);
 				}
 				this.debug("built vector " + i, vector);
-
 				data.push(vector);
 			}
 			return data;
