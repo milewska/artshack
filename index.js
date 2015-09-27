7386eaeb9f99b027131e0bf2804c5a15d0459da5
cesine <cesine@yahoo.com>
2015-09-26 19:08:06 -0600

    jshint

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
 
 app.use(bodyParser.json({ limit: "5000kb" }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res, next) {
+app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
 var shipFrame = _.throttle(function(data) {
-	io.emit('frame', data);
+	io.emit("frame", data);
 }, 1000/30);
 
-app.post("/frame", function(req, res, next) {
+app.post("/frame", function(req, res) {
 	shipFrame(req.body);
 	res.send({ ok: true });
 });
@@ -25,3 +26,5 @@ app.post("/frame", function(req, res, next) {
 server.listen(process.env.PORT || 3000, function() {
 	console.log("ready");
 });
+
+
